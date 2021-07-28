cdcf8f8f651ba6c60ca87ae33a657590086c58e3
kshalm <lynden.shalm@colorado.edu>
2021-07-28 15:48:58 -0600

    first commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+## ZMQ Helpers
+
+Simple python zmq classes for creating multithreaded server with workers and a client that can poll.
---
+++ zmqhelpers/server.py
@@ -0,0 +1,48 @@
+import zmq
+import threading
+
+
+class Server():
+    def __init__(self, port, n_workers=4):
+        url_worker = "inproc://workers"
+        url_client = 'tcp://*:'+port
+        self.context = zmq.Context.instance()
+        # Socket to talk to clients
+        clients = self.context.socket(zmq.ROUTER)
+        clients.bind(url_client)
+
+        workers = self.context.socket(zmq.DEALER)
+        workers.bind(url_worker)
+
+        self.workers = []
+        for k in range(0, n_workers):
+            self.workers.append(threading.Thread(target=self.start_worker, args=(url_worker,k,)))
+            self.workers[-1].start()
+
+        zmq.proxy(clients, workers)
+        # We never get here but clean up anyhow
+        clients.close()
+        workers.close()
+        context.term()
+
+
+    def start_worker(self, url_worker, k=0):
+        """Worker routine"""
+        context = self.context or zmq.Context.instance()
+        # Socket to talk to dispatcher
+        socket = context.socket(zmq.REP)
+        socket.connect(url_worker)
+
+        while True:
+            message = socket.recv()
+            
+            response = self.handle(message)
+            # print('worker '+str(k)+' reporting for duty, '+response.decode())
+            socket.send(response)
+
+
+    def handle(self, message):
+        return message
+
+if __name__ == '__main__':
+    zmqs = Server('5555', n_workers=4)
